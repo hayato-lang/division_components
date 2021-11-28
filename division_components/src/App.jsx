c158721700006026a4d1ff932a1160768da861f4
hayato-lang <j.h.14.s.g.8@gmail.com>
2021-11-29 08:44:02 +0900

    atomの作成

---
+++ division_components/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import { PrimaryButton } from './components/atoms/button/PrimaryButton';
+import { SecondaryButton } from './components/atoms/button/SecondaryButton';
+
+function App() {
+  return (
+    <div className="App">
+      <PrimaryButton>テスト</PrimaryButton>
+      <SecondaryButton>検索</SecondaryButton>
+    </div>
+  );
+}
+
+export default App;
